f6052404b1ce96b25ff2511726d961c7d0ec891f
Paula Rodrigues <paulinhaabro@gmail.com>
2022-07-20 17:58:26 -0300

    implement routes and Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import Home from "./components/home/Home";
 import Quiz from "./components/quiz/Quiz";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,21 +2,51 @@ import "./Home.css";
 
 import decks from "../../utils/decks";
 import Logo from "../logo/Logo";
-import {useRef, useState} from "react";
-import Dropdown from "../dropdown/Dropdown";
+import {useEffect, useRef, useState} from "react";
+import {Link} from "react-router-dom";
 
 export default function Home() {
-  const [disable, setDisable] = useState(false);
+  const [disable, setDisable] = useState(true);
   const [zapGoal, setZapGoal] = useState("");
   const [deck, setDeck] = useState("");
   const zapRef = useRef("");
   const deckRef = useRef("");
 
+  function enableButton() {
+    setDisable(!(deck && zapGoal));
+  }
+
+  useEffect(() => {
+    enableButton();
+    zapRef.current = zapGoal;
+  }, [zapGoal]);
+
+  useEffect(() => {
+    enableButton();
+    deckRef.current = deck;
+  }, [deck]);
+
   return (
     <main className="home">
       <Logo />
-      <Dropdown decks={Object.keys(decks)} deck={deck} setDeck={setDeck} />
-      <input type="text" className="input" placeholder="Digite sua meta de zaps..." />
+      <select ref={deckRef} onChange={e => setDeck(e.target.value)}>
+        <option selected disabled>
+          Escolha seu deck
+        </option>
+        {Object.keys(decks).map(deck => (
+          <option>{deck}</option>
+        ))}
+      </select>
+      <input
+        ref={zapRef}
+        type="text"
+        onChange={e => setZapGoal(e.target.value)}
+        placeholder="Digite sua meta de zaps..."
+      />
+
+      <Link to="/quiz">
+        <button disabled={disable}>Iniciar Recall!</button>
+      </Link>
     </main>
   );
 }
