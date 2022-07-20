6fa4529f96f068986fcf30c207aafd2297911014
Paula Rodrigues <paulinhaabro@gmail.com>
2022-07-20 11:04:29 -0300

    change decks

--- src/utils/decks.js
+++ src/utils/decks.js
@@ -1,425 +1,138 @@
 const decks = [
   {
-    category: "Simple Past",
-    decks: {
-      easy: [
-        {
-          question: "say",
-          answer: "said",
-        },
-        {
-          question: "make",
-          answer: "made",
-        },
-        {
-          question: "go",
-          answer: "went",
-        },
-        {
-          question: "take",
-          answer: "took",
-        },
-        {
-          question: "come",
-          answer: "came",
-        },
-        {
-          question: "see",
-          answer: "saw",
-        },
-        {
-          question: "know",
-          answer: "knew",
-        },
-        {
-          question: "get",
-          answer: "got",
-        },
-        {
-          question: "give",
-          answer: "gave",
-        },
-        {
-          question: "find",
-          answer: "found",
-        },
-        {
-          question: "think",
-          answer: "thought",
-        },
-        {
-          question: "tell",
-          answer: "told",
-        },
-        {
-          question: "become",
-          answer: "became",
-        },
-        {
-          question: "show",
-          answer: "showed",
-        },
-        {
-          question: "leave",
-          answer: "left",
-        },
-      ],
-      medium: [
-        {
-          question: "feel",
-          answer: "felt",
-        },
-        {
-          question: "put",
-          answer: "put",
-        },
-        {
-          question: "bring",
-          answer: "brought",
-        },
-        {
-          question: "begin",
-          answer: "began",
-        },
-        {
-          question: "keep",
-          answer: "kept",
-        },
-        {
-          question: "hold",
-          answer: "held",
-        },
-        {
-          question: "write",
-          answer: "wrote",
-        },
-        {
-          question: "stand",
-          answer: "stood",
-        },
-        {
-          question: "hear",
-          answer: "heard",
-        },
-        {
-          question: "let",
-          answer: "let",
-        },
-        {
-          question: "mean",
-          answer: "meant",
-        },
-        {
-          question: "set",
-          answer: "set",
-        },
-        {
-          question: "meet",
-          answer: "met",
-        },
-        {
-          question: "run",
-          answer: "ran",
-        },
-        {
-          question: "pay",
-          answer: "paid",
-        },
-      ],
-      hard: [
-        {
-          question: "sit",
-          answer: "sat",
-        },
-        {
-          question: "speak",
-          answer: "spoke",
-        },
-        {
-          question: "lie",
-          answer: "lay",
-        },
-        {
-          question: "lead",
-          answer: "led",
-        },
-        {
-          question: "read",
-          answer: "read",
-        },
-        {
-          question: "grow",
-          answer: "grew",
-        },
-        {
-          question: "lose",
-          answer: "lost",
-        },
-        {
-          question: "fall",
-          answer: "fell",
-        },
-        {
-          question: "send",
-          answer: "sent",
-        },
-        {
-          question: "build",
-          answer: "built",
-        },
-        {
-          question: "understand",
-          answer: "understood",
-        },
-        {
-          question: "draw",
-          answer: "drew",
-        },
-        {
-          question: "break",
-          answer: "broke",
-        },
-        {
-          question: "spend",
-          answer: "spent",
-        },
-        {
-          question: "cut",
-          answer: "cut",
-        },
-        {
-          question: "rise",
-          answer: "rose",
-        },
-        {
-          question: "drive",
-          answer: "drove",
-        },
-        {
-          question: "buy",
-          answer: "bought",
-        },
-        {
-          question: "wear",
-          answer: "wore",
-        },
-        {
-          question: "choose",
-          answer: "chose",
-        },
-      ],
-    },
-  },
-  {
-    category: "Past Participle",
-    decks: {
-      easy: [
-        {
-          question: "say",
-          answer: "said",
-        },
-        {
-          question: "make",
-          answer: "made",
-        },
-        {
-          question: "go",
-          answer: "gone",
-        },
-        {
-          question: "take",
-          answer: "taken",
-        },
-        {
-          question: "come",
-          answer: "come",
-        },
-        {
-          question: "see",
-          answer: "seen",
-        },
-        {
-          question: "know",
-          answer: "known",
-        },
-        {
-          question: "get",
-          answer: "got/gotten (US)",
-        },
-        {
-          question: "give",
-          answer: "given",
-        },
-        {
-          question: "find",
-          answer: "found",
-        },
-        {
-          question: "think",
-          answer: "thought",
-        },
-        {
-          question: "tell",
-          answer: "told",
-        },
-        {
-          question: "become",
-          answer: "become",
-        },
-        {
-          question: "show",
-          answer: "shown",
-        },
-        {
-          question: "leave",
-          answer: "left",
-        },
-      ],
-      medium: [
-        {
-          question: "feel",
-          answer: "felt",
-        },
-        {
-          question: "put",
-          answer: "put",
-        },
-        {
-          question: "bring",
-          answer: "brought",
-        },
-        {
-          question: "begin",
-          answer: "begun",
-        },
-        {
-          question: "keep",
-          answer: "kept",
-        },
-        {
-          question: "hold",
-          answer: "held",
-        },
-        {
-          question: "write",
-          answer: "written",
-        },
-        {
-          question: "stand",
-          answer: "stood",
-        },
-        {
-          question: "hear",
-          answer: "heard",
-        },
-        {
-          question: "let",
-          answer: "let",
-        },
-        {
-          question: "mean",
-          answer: "meant",
-        },
-        {
-          question: "set",
-          answer: "set",
-        },
-        {
-          question: "meet",
-          answer: "met",
-        },
-        {
-          question: "run",
-          answer: "run",
-        },
-        {
-          question: "pay",
-          answer: "paid",
-        },
-      ],
-      hard: [
-        {
-          question: "sit",
-          answer: "sat",
-        },
-        {
-          question: "speak",
-          answer: "spoken",
-        },
-        {
-          question: "lie",
-          answer: "lain",
-        },
-        {
-          question: "lead",
-          answer: "led",
-        },
-        {
-          question: "read",
-          answer: "read",
-        },
-        {
-          question: "grow",
-          answer: "grown",
-        },
-        {
-          question: "lose",
-          answer: "lost",
-        },
-        {
-          question: "fall",
-          answer: "fallen",
-        },
-        {
-          question: "send",
-          answer: "sent",
-        },
-        {
-          question: "build",
-          answer: "built",
-        },
-        {
-          question: "understand",
-          answer: "understood",
-        },
-        {
-          question: "draw",
-          answer: "drawn",
-        },
-        {
-          question: "break",
-          answer: "broken",
-        },
-        {
-          question: "spend",
-          answer: "spent",
-        },
-        {
-          question: "cut",
-          answer: "cut",
-        },
-        {
-          question: "rise",
-          answer: "risen",
-        },
-        {
-          question: "drive",
-          answer: "driven",
-        },
-        {
-          question: "buy",
-          answer: "bought",
-        },
-        {
-          question: "wear",
-          answer: "worn",
-        },
-        {
-          question: "choose",
-          answer: "chosen",
-        },
-      ],
-    },
+    Terminal: [
+      {
+        question: "Como ir de qualquer diretório para o home do usuário?",
+        answer: "cd",
+      },
+      {
+        question: "Como voltar para o diretório pai?",
+        answer: "cd ..",
+      },
+      {
+        question: "Como ir para um diretório filho?",
+        answer: "cd <dirname>",
+      },
+      {
+        question: "Qual comando mostra o path atual?",
+        answer: "pwd",
+      },
+      {
+        question: "Qual comando mostra o nome do usuário?",
+        answer: "whoami",
+      },
+      {
+        question: "Como criar um diretório vazio?",
+        answer: "mkdir <dirname>",
+      },
+      {
+        question: "Como remover um diretório vazio?",
+        answer: "rmdir <dirname>",
+      },
+      {
+        question: "Como criar um arquivo?",
+        answer: "touch <filename>",
+      },
+      {
+        question: "Qual comando te deixa ver o conteúdo do arquivo direto no terminal?",
+        answer: "cat <filename>",
+      },
+      {
+        question: "Como mostrar no terminal as primeiras N linhas de um arquivo?",
+        answer: "head -n N <filename>",
+      },
+      {
+        question: "Como mostrar no terminal as últimas N linhas de um arquivo?",
+        answer: "tail -n N <filename>",
+      },
+      {
+        question: "Qual comando deleta uma pasta e seus conteúdos?",
+        answer: "rm -r <dirname>",
+      },
+      {
+        question: "Qual comando remove um arquivo?",
+        answer: "rm <filename>",
+      },
+      {
+        question: "Qual símbolo representa a home?",
+        answer: "~",
+      },
+      {
+        question: "Qual símbolo representa o diretório atual?",
+        answer: ".",
+      },
+      {
+        question: "Qual símbolo representa o diretório pai?",
+        answer: "..",
+      },
+      {
+        question: "Como renomear arquivos/diretórios?",
+        answer: "mv <oldname> <newname>",
+      },
+      {
+        question: "Como mover arquivos/diretórios?",
+        answer: "mv <name> <existingfolder>",
+      },
+      {
+        question: "Como copiar arquivos?",
+        answer: "cp <filename> <copyname>",
+      },
+      {
+        question: "Como copiar pastas?",
+        answer: "cp -r <filename> <copyname>",
+      },
+      {
+        question: "Como imprimir 'Hello World!' no terminal?",
+        answer: "echo 'Hello World!",
+      },
+      {
+        question: "Como salvar 'Hello World!' em 'hello.txt'?",
+        answer: "echo 'Hello World! > hello.txt",
+      },
+      {
+        question: "Como limpar o terminal?",
+        answer: "clear",
+      },
+      {
+        question: "Como parar execução de um código?",
+        answer: "CTRL + C",
+      },
+      {
+        question: "Como fechar o terminal?",
+        answer: "exit",
+      },
+    ],
+    Git: [
+      {
+        question: "Como iniciar um repositório git?",
+        answer: "git init",
+      },
+      {
+        question:
+          "Em qual arquivo deve-se colocar o nome de pastas/arquivos a serem ignorados pelo git?",
+        answer: ".gitignore",
+      },
+      {
+        question: "Quais os passos para adicionar mudanças ao repo no GitHub?",
+        answer: "git add <filename>\ngit commit -m 'mensagem'\ngit push",
+      },
+      {
+        question: "Qual o comando para adicionar um arquivo a área de staging?",
+        answer: "git add <filename>",
+      },
+      {
+        question: "Qual o commando para commitar as mudanças?",
+        answer: "git commit -m 'mensagem'",
+      },
+      {
+        question: "Como ver o histórico de commits?",
+        answer: "git log",
+      },
+      {
+        question: "Como ver a área de staging?",
+        answer: "git status",
+      },
+    ],
   },
 ];
 
