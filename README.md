f91a6a5eb192ecefd6ae0c2d203cfcf61b12901c
Paula Rodrigues <paulinhaabro@gmail.com>
2022-07-20 01:24:42 -0300

    :tada: configure file for project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto 8 - Zap Recall
+
+> Você pode visualizar a página [aqui](https://zaprecall.driven-paulaabro.com).
---
+++ src/utils/decks.js
@@ -0,0 +1,426 @@
+const decks = [
+  {
+    category: "Simple Past",
+    decks: {
+      easy: [
+        {
+          question: "say",
+          answer: "said",
+        },
+        {
+          question: "make",
+          answer: "made",
+        },
+        {
+          question: "go",
+          answer: "went",
+        },
+        {
+          question: "take",
+          answer: "took",
+        },
+        {
+          question: "come",
+          answer: "came",
+        },
+        {
+          question: "see",
+          answer: "saw",
+        },
+        {
+          question: "know",
+          answer: "knew",
+        },
+        {
+          question: "get",
+          answer: "got",
+        },
+        {
+          question: "give",
+          answer: "gave",
+        },
+        {
+          question: "find",
+          answer: "found",
+        },
+        {
+          question: "think",
+          answer: "thought",
+        },
+        {
+          question: "tell",
+          answer: "told",
+        },
+        {
+          question: "become",
+          answer: "became",
+        },
+        {
+          question: "show",
+          answer: "showed",
+        },
+        {
+          question: "leave",
+          answer: "left",
+        },
+      ],
+      medium: [
+        {
+          question: "feel",
+          answer: "felt",
+        },
+        {
+          question: "put",
+          answer: "put",
+        },
+        {
+          question: "bring",
+          answer: "brought",
+        },
+        {
+          question: "begin",
+          answer: "began",
+        },
+        {
+          question: "keep",
+          answer: "kept",
+        },
+        {
+          question: "hold",
+          answer: "held",
+        },
+        {
+          question: "write",
+          answer: "wrote",
+        },
+        {
+          question: "stand",
+          answer: "stood",
+        },
+        {
+          question: "hear",
+          answer: "heard",
+        },
+        {
+          question: "let",
+          answer: "let",
+        },
+        {
+          question: "mean",
+          answer: "meant",
+        },
+        {
+          question: "set",
+          answer: "set",
+        },
+        {
+          question: "meet",
+          answer: "met",
+        },
+        {
+          question: "run",
+          answer: "ran",
+        },
+        {
+          question: "pay",
+          answer: "paid",
+        },
+      ],
+      hard: [
+        {
+          question: "sit",
+          answer: "sat",
+        },
+        {
+          question: "speak",
+          answer: "spoke",
+        },
+        {
+          question: "lie",
+          answer: "lay",
+        },
+        {
+          question: "lead",
+          answer: "led",
+        },
+        {
+          question: "read",
+          answer: "read",
+        },
+        {
+          question: "grow",
+          answer: "grew",
+        },
+        {
+          question: "lose",
+          answer: "lost",
+        },
+        {
+          question: "fall",
+          answer: "fell",
+        },
+        {
+          question: "send",
+          answer: "sent",
+        },
+        {
+          question: "build",
+          answer: "built",
+        },
+        {
+          question: "understand",
+          answer: "understood",
+        },
+        {
+          question: "draw",
+          answer: "drew",
+        },
+        {
+          question: "break",
+          answer: "broke",
+        },
+        {
+          question: "spend",
+          answer: "spent",
+        },
+        {
+          question: "cut",
+          answer: "cut",
+        },
+        {
+          question: "rise",
+          answer: "rose",
+        },
+        {
+          question: "drive",
+          answer: "drove",
+        },
+        {
+          question: "buy",
+          answer: "bought",
+        },
+        {
+          question: "wear",
+          answer: "wore",
+        },
+        {
+          question: "choose",
+          answer: "chose",
+        },
+      ],
+    },
+  },
+  {
+    category: "Past Participle",
+    decks: {
+      easy: [
+        {
+          question: "say",
+          answer: "said",
+        },
+        {
+          question: "make",
+          answer: "made",
+        },
+        {
+          question: "go",
+          answer: "gone",
+        },
+        {
+          question: "take",
+          answer: "taken",
+        },
+        {
+          question: "come",
+          answer: "come",
+        },
+        {
+          question: "see",
+          answer: "seen",
+        },
+        {
+          question: "know",
+          answer: "known",
+        },
+        {
+          question: "get",
+          answer: "got/gotten (US)",
+        },
+        {
+          question: "give",
+          answer: "given",
+        },
+        {
+          question: "find",
+          answer: "found",
+        },
+        {
+          question: "think",
+          answer: "thought",
+        },
+        {
+          question: "tell",
+          answer: "told",
+        },
+        {
+          question: "become",
+          answer: "become",
+        },
+        {
+          question: "show",
+          answer: "shown",
+        },
+        {
+          question: "leave",
+          answer: "left",
+        },
+      ],
+      medium: [
+        {
+          question: "feel",
+          answer: "felt",
+        },
+        {
+          question: "put",
+          answer: "put",
+        },
+        {
+          question: "bring",
+          answer: "brought",
+        },
+        {
+          question: "begin",
+          answer: "begun",
+        },
+        {
+          question: "keep",
+          answer: "kept",
+        },
+        {
+          question: "hold",
+          answer: "held",
+        },
+        {
+          question: "write",
+          answer: "written",
+        },
+        {
+          question: "stand",
+          answer: "stood",
+        },
+        {
+          question: "hear",
+          answer: "heard",
+        },
+        {
+          question: "let",
+          answer: "let",
+        },
+        {
+          question: "mean",
+          answer: "meant",
+        },
+        {
+          question: "set",
+          answer: "set",
+        },
+        {
+          question: "meet",
+          answer: "met",
+        },
+        {
+          question: "run",
+          answer: "run",
+        },
+        {
+          question: "pay",
+          answer: "paid",
+        },
+      ],
+      hard: [
+        {
+          question: "sit",
+          answer: "sat",
+        },
+        {
+          question: "speak",
+          answer: "spoken",
+        },
+        {
+          question: "lie",
+          answer: "lain",
+        },
+        {
+          question: "lead",
+          answer: "led",
+        },
+        {
+          question: "read",
+          answer: "read",
+        },
+        {
+          question: "grow",
+          answer: "grown",
+        },
+        {
+          question: "lose",
+          answer: "lost",
+        },
+        {
+          question: "fall",
+          answer: "fallen",
+        },
+        {
+          question: "send",
+          answer: "sent",
+        },
+        {
+          question: "build",
+          answer: "built",
+        },
+        {
+          question: "understand",
+          answer: "understood",
+        },
+        {
+          question: "draw",
+          answer: "drawn",
+        },
+        {
+          question: "break",
+          answer: "broken",
+        },
+        {
+          question: "spend",
+          answer: "spent",
+        },
+        {
+          question: "cut",
+          answer: "cut",
+        },
+        {
+          question: "rise",
+          answer: "risen",
+        },
+        {
+          question: "drive",
+          answer: "driven",
+        },
+        {
+          question: "buy",
+          answer: "bought",
+        },
+        {
+          question: "wear",
+          answer: "worn",
+        },
+        {
+          question: "choose",
+          answer: "chosen",
+        },
+      ],
+    },
+  },
+];
+
+export default decks;
