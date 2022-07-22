1770e19dba4ecff87313dab1105bee7c9951edf8
Paula Rodrigues <paulinhaabro@gmail.com>
2022-07-21 23:25:31 -0300

    middle of component Message

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,6 +1,23 @@
 import "./Message.css";
 
 export default function Message({results, zapGoal}) {
+  function countZap(list) {
+    let zapCount = 0;
+    for (const el of list) {
+      if (el === "checkmark-circle") {
+        zapCount++;
+      }
+    }
+    return zapCount;
+  }
+  if (countZap(results) >= zapGoal) {
+    return (
+      <div className="message">
+        <h3></h3>
+      </div>
+    );
+  }
+
   return (
     <div className="message">
       <h3></h3>
